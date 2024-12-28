c7389ccb3dace8574fa6c11efe0ca9cc4c484af3
Efferusroxxa <efferusroxxa632@gmail.com>
2024-12-28 16:34:32 +0600

    Variable Let

---
+++ 02-variable-let.js
@@ -0,0 +1,26 @@
+// var husbanOfJamilasName;
+// husbanOfJamilasName = "Yousuf";
+// console.log(husbanOfJamilasName);
+
+// var husbanOfJamilasName = "Yousuf";
+// console.log(husbanOfJamilasName);
+
+// let husbanOfJamilasName
+// husbanOfJamilasName = "Yousuf";
+// console.log(husbanOfJamilasName);
+
+// let husbanOfJamilasName = "Yousuf";
+// console.log(husbanOfJamilasName);
+
+
+// var husbanOfJamilasName = "Yousuf";
+// console.log(husbanOfJamilasName);
+// var husbanOfJamilasName = "Yousuf Jamila";
+// console.log(husbanOfJamilasName);
+
+// let husbanOfJamilasName = "Yousuf";
+//  console.log(husbanOfJamilasName);
+//  let husbanOfJamilasName = "Yousuf Jamila";
+// console.log(husbanOfJamilasName);
+// husbanOfJamilasName = "Yousuf Jamila";
+// console.log(husbanOfJamilasName);
