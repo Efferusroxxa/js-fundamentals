61f4646de943d3881bcf5a17d6fbf555cb6ae09f
Efferusroxxa <efferusroxxa632@gmail.com>
2024-12-28 19:43:14 +0600

    Object n Array

---
+++ 04-Data-Types.js
@@ -0,0 +1,82 @@
+//  const heroName ="Allu Arjun"; //String
+// console.log(heroName)
+// let heroName;
+/*
+JS -> Two Types of Data Type
+1. Primitive -> Immutable -> Unchangable
+2. Non Primitive -> Mutable -> Changable
+
+1. Primitive Data Types -> Seven Types of Data Types.
+1.1: String -> "Jamila", 'Jamila', `Jamila`
+1.2: Number -> 1, 0.5, ΝΑΝ
+1.3: Boolean -> true, false
+1.4: Null -> null
+1.5: Undefined -> undefined
+
+// We are not discussing about this two topics -> Advanced Topics
+1.6: Symbol
+1.7: BigInt
+
+2. NonPremative Data Types -> Three Types Of Data Types
+2.1: Object ->
+2.2: Array ->
+2.3: Function ->
+*/
+
+
+// let name ="John"; //Statement
+// console.log(name)
+// let greeting ='Hello!'
+// console.log(greeting)
+// let message =` ${greeting} Welcome ${name}`;
+// ${} -> Expression ->Single Value
+// console.log(message)
+
+
+// Number Data Types
+// let age = 25;
+// // console.log(age)
+// let Temprature = 36.6;
+// console.log(Temprature)
+// console.log(typeof Temprature)
+
+// Data Types Boolean
+// let isElegibleForMarriage =true;
+// console.log(isElegibleForMarriage);
+// console.log(typeof isElegibleForMarriage);
+// let isStudent =false;
+// console.log(isStudent)
+
+//Data Types Null
+let YousufWifeName =null;
+console.log(YousufWifeName);
+console.log(typeof YousufWifeName);
+
+//Data Types - Undefined
+// let notAssigned;
+// console.log(notAssigned);
+// console.log(typeof notAssigned);
+
+//NonPremative Data Types => Object
+// let mohan = {
+//     //Properties -> Key And Value
+//     fullname: "Mohan Lal",
+//     // Key : Every Data Types = Prosperties,
+//       Age: 20,
+//       isStudent: true,
+//       isJobHolder: false,
+//       isHeMarried: "Yes",
+// };
+
+// console.log(mohan)
+// console.log(mohan.fullname)
+// console.log(mohan.isHeMarried)
+// Dot Notation
+// console.log(mohan["fullname"])
+//Bracket Notation
+
+//NonPremative Data Types => Array
+let friends = ["abdul", "kuddus" ,"jobbar", true , {fullName:"aleem"} ];
+//                0         1         2       3 4
+// console.log(friends);
+console.log(friends[4])
