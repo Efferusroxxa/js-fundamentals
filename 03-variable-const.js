5157e71dc67e6b0261ab6c9c635be39bf9d07a3d
Efferusroxxa <efferusroxxa632@gmail.com>
2024-12-28 17:23:57 +0600

    Const

---
+++ 03-variable-const.js
@@ -0,0 +1,17 @@
+// const wifeOfYousuf = "Jamila Yousuf";
+// console.log(wifeOfYousuf);
+
+const wifeOfYousuf = "Jamila Yousuf";
+// console.log(wifeOfYousuf);
+// wifeOfYousuf = "Mehjabin Yousuf";
+console.log(wifeOfYousuf);
+
+/*
+ var -> Initialization, Declare, Reassign, Redeclare
+ let -> Initialization, Declare, Reassign
+ const -> Initialization
+
+  NamingConvention -> CamelCase a-z, A-Z, 0-9, $, _
+  Spacing, CaseSensitive -> A-a
+*/  
+
